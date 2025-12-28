5f0cd155ccd89e736c871feba15120f058e42e64
st <0911324@gmail.com>
2025-12-28 21:28:05 +0800

    add integration test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-   HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind");
+    run(listener)?.await
 }
